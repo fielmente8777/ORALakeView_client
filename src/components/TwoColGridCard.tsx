8e094e70b0430dd66c0a15d3cef5d3794508ec44
abhijeet <abhijeet@eazotel.com>
2025-02-18 15:24:09 +0530

    up

--- src/components/TwoColGridCard.tsx
+++ src/components/TwoColGridCard.tsx
@@ -59,7 +59,7 @@ const TwoColGridCard: React.FC<TwoColGridCardProps> = ({
             </Paragraph>
 
             <Button
-              href="/contact-us"
+              href="https://direct-book.com/properties/lakeviewhotelrestaurant?locale=en&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=CHF&checkInDate=2025-01-31&checkOutDate=2025-02-01&trackPage=yes"
               label="CHECK AVAILABILITY"
               className="mt-2 w-fit bg-quaternary hover:bg-quaternary/80"
             />
--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,4 @@
-export const bookingUrl = "";
+export const bookingUrl = "https://direct-book.com/properties/lakeviewhotelrestaurant?locale=en&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=CHF&checkInDate=2025-01-31&checkOutDate=2025-02-01&trackPage=yes";
 
 export const homeImages = "https://eazotel-client-webp-image.s3.ap-south-1.amazonaws.com/ora-lake-view/home";
 
