4421b51aea44a8d9cc828a1feacc5ac334a333d0
sumit <sk7627577@gmail.com>
2025-04-16 16:56:49 +0530

    refactor: comment out unused config options in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  trailingSlash: true,
-  reactStrictMode: false,
+  // output: "export",
   images: {
-    unoptimized: true,
+    // unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
+      },
+    ],
+    dangerouslyAllowSVG: true,
   },
+  // trailingSlash: true,
 };
 
 export default nextConfig;
